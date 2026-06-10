0ac10273a5e82455b8d4177202904052d5bd7365
Dillon McCloud <dillon@sapior.ai>
2026-06-10 16:12:38 -0700

    setting up routes controllers modules services

---
+++ src/users/users.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  // GET /users
+  @Get()
+  findAll() {
+    return [];
+  }
+
+  // GET /users/:id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  // POST /users
+  @Post()
+  createUser(@Body() user: {}) {
+    return user;
+  }
+
+  // PATCH /users/:id
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  // DELETE /users/:id
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return {};
+  }
+}
